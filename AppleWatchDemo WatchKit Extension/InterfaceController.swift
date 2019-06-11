8ae8c0f04f0f3d30a58c324e5044eb4c45d78f7f
Girijesh Mac2 <>
2019-06-11 21:29:16 +0530

    Apple Watch API integrated

--- AppleWatchDemo WatchKit Extension/InterfaceController.swift
+++ AppleWatchDemo WatchKit Extension/InterfaceController.swift
@@ -10,34 +10,13 @@ import WatchKit
 import Foundation
 import WatchConnectivity
 
-// identifier: page Interface Controller identifier.
-// Context: page context, a string used as the action button title.
-//
-struct ControllerID {
-    static let mainInterfaceController = "MainInterfaceController"
-}
+class InterfaceController: WKInterfaceController{
 
-class InterfaceController: WKInterfaceController, TestDataProvider, SessionCommands  {
-
-    // Retain the controllers so that we don't have to reload root controllers for every switch.
-    //
-    static var instances = [InterfaceController]()
-    private var command: Command!
-    
     @IBOutlet var statusLabel: WKInterfaceLabel!
     @IBOutlet var commandButton: WKInterfaceButton!
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
-        if let context = context as? CommandStatus {
-            command = context.command
-            updateUI(with: context)
-            type(of: self).instances.append(self)
-        } else {
-            statusLabel.setText("Activating...")
-        }
         
         // Install notification observer.
         //
@@ -58,20 +37,7 @@ class InterfaceController: WKInterfaceController, TestDataProvider, SessionComma
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        
-        guard command != nil else { return } // For first-time loading do nothing.
-        
-        // For .updateAppContext, retrieve the receieved app context if any and update the UI.
-        // For .transferFile and .transferUserInfo, log the outstanding transfers if any.
-        //
-        if command == .updateAppContext {
-            let timedColor = WCSession.default.receivedApplicationContext
-            if timedColor.isEmpty == false {
-                var commandStatus = CommandStatus(command: command, phrase: .received)
-                commandStatus.timedColor = TimedColor(timedColor)
-                updateUI(with: commandStatus)
-            }
-        }
+    
     }
     
     override func didDeactivate() {
@@ -83,15 +49,7 @@ class InterfaceController: WKInterfaceController, TestDataProvider, SessionComma
     //
     @objc
     func dataDidFlow(_ notification: Notification) {
-        guard let commandStatus = notification.object as? CommandStatus else { return }
-        
-        // If the data is from current channel, simple update color and time stamp, then return.
-        //
-        if commandStatus.command == command {
-            updateUI(with: commandStatus, errorMessage: commandStatus.errorMessage)
-            return
-        }
-        
+    
     }
     
     // .activationDidComplete notification handler.
@@ -108,32 +66,50 @@ class InterfaceController: WKInterfaceController, TestDataProvider, SessionComma
         print("\(#function): isReachable:\(WCSession.default.isReachable)")
     }
     
-    // Update the user interface with the command status.
-    // Note that there isn't a timed color when the interface controller is initially loaded.
-    //
-    private func updateUI(with commandStatus: CommandStatus, errorMessage: String? = nil) {
-        guard let timedColor = commandStatus.timedColor else {
-            statusLabel.setText("")
-            commandButton.setTitle(commandStatus.command.rawValue)
-            return
-        }
+    private func updateUI(success:Bool,message:String){
+        
+        self.statusLabel.setText(message)
+
+        let color:UIColor = success ? .white : .red
+        
+        self.statusLabel.setTextColor(color)
     }
     
     // Do the command associated with the current page.
     //
     @IBAction func commandAction() {
         
-        updateAppContext(appContext)
+        let urlStr = URL(string: "http://yin2.schwarzsoftware.com.au/cgi-bin/hello_world.py")
         
-//        guard let command1 = command else { return }
-//
-//        switch command1 {
-//        case .updateAppContext: updateAppContext(appContext)
-//        case .sendMessage: sendMessage(message)
-//        case .sendMessageData: sendMessageData(messageData)
-//        case .transferUserInfo: transferUserInfo(userInfo)
-//        case .transferFile: transferFile(file, metadata: fileMetaData)
-//        case .transferCurrentComplicationUserInfo: transferCurrentComplicationUserInfo(currentComplicationInfo)
-//        }
+        let stringPost="timestamp=\(Date().iso8601)" // Key and Value
+
+        var request = URLRequest(url: urlStr! , cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 60)
+        
+        request.httpBody = stringPost.data(using: .utf8)
+        request.httpMethod = "POST" // POST ,GET, PUT What you want
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: request as URLRequest) {data,response,error in
+            
+            do {
+                
+                if let respdata = data, let jsonResult = try JSONSerialization.jsonObject(with: respdata, options: .mutableContainers) as? [String:Any]{
+                    
+                    if let sucess = jsonResult["success"] as? Bool,
+                        let msg = jsonResult["message"] as? String{
+                        self.updateUI(success: sucess, message: msg)
+                    }
+                    
+                }
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        dataTask.resume()
+
     }
+    
 }
